82c4b04fcd33164e2d0fe74f1e9d9959526e3a3d
Danrave Cabahug Keh <danravekeh123@gmail.com>
2025-01-24 21:02:12 +0800

    initial commit

---
+++ BackEnd/src/test/java/com/Storix/StorixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Storix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-red-500">Tailwind Added
+      </p>
+    </>
+  )
+}
+
+export default App
