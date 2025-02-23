f27e6296c598e49b0039d7594ecec499dacb9e29
bean_sint <123951763+beansint@users.noreply.github.com>
2025-02-23 18:05:37 +0800

    Renamed main and test java files from Storix into Judify

--- backend/src/main/java/edu/cit/Judify/JudifyApplication.java
+++ backend/src/main/java/edu/cit/Judify/JudifyApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StorixApplication {
+public class JudifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StorixApplication.class, args);
+		SpringApplication.run(JudifyApplication.class, args);
 	}
-
 }
